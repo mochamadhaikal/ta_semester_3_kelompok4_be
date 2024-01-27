fb22e47562631292fd806ac8ca090b5bece2e715
haikalsalam <mochamad.haikal@salamenterprise.com>
2024-01-27 10:50:43 +0700

    feat(loan): update query all and one

--- src/modules/loan/loan.service.ts
+++ src/modules/loan/loan.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Loan } from './entities';
 import { LoanDetailDTO, LoanCreateDTO, LoanUpdateDTO } from './dto';
 import { Res } from 'src/configs';
@@ -10,17 +10,55 @@ export class LoanService {
   constructor(
     @InjectRepository(Loan) // Replace with your actual Loan
     private loanRepository: Repository<Loan>,
+    private dataSource: DataSource,
   ) {}
-  findAll(): Promise<Loan[]> {
-    return this.loanRepository.find();
+  async findAll() {
+    const entityManager = this.dataSource.createEntityManager();
+    const data = await entityManager.query(`
+    SELECT 
+      loan.loan_id as loanId,
+      friend.friend_id as friendId,
+      dvd.dvd_id as dvdId,
+      friend.name as friendName,
+      dvd.title as dvdName,
+      CONVERT(loan.loan_date, CHAR) AS loanDate,
+      CONVERT(loan.return_date, CHAR) AS returnDate
+    FROM loan
+    INNER JOIN dvd ON loan.dvd_id = dvd.dvd_id
+    INNER JOIN friend ON loan.friend_id = friend.friend_id;
+    `);
+
+    return data;
   }
 
-  findOne(item: LoanDetailDTO): Promise<Loan> {
+  async findOne(item: LoanDetailDTO) {
     try {
       console.log('Success Get Loan Detail with Id:', item.loanId);
-      return this.loanRepository.findOne({
-        where: { loanId: item.loanId },
-      });
+      const entityManager = this.dataSource.createEntityManager();
+      const data = await entityManager.query(
+        `
+      SELECT 
+        loan.loan_id as loanId,
+        friend.friend_id as friendId,
+        dvd.dvd_id as dvdId,
+        friend.name as friendName,
+        dvd.title as dvdName,
+        CONVERT(loan.loan_date, CHAR) AS loanDate,
+        CONVERT(loan.return_date, CHAR) AS returnDate
+      FROM loan
+      INNER JOIN dvd ON loan.dvd_id = dvd.dvd_id
+      INNER JOIN friend ON loan.friend_id = friend.friend_id
+      WHERE loan.loan_id=$1
+      `,
+        [item.loanId],
+      );
+      if (data.length === 0) {
+        throw new Error('Loan id ' + item.loanId + ' not found');
+      }
+      // return this.loanRepository.findOne({
+      //   where: { loanId: item.loanId },
+      // });
+      return data;
     } catch (error) {
       console.log('Err:', error.message);
     }
@@ -41,6 +79,8 @@ export class LoanService {
         };
       }
       await this.loanRepository.insert(item);
+      console.log('Success Create Loan');
+
       return {
         status: true,
         message: 'Success Created Data!',
